8d99d3380af79780f555eb0b0d8cd9e9478509c5
Bxdda (Chaughn) <cr1001404@wcupa.edu>
2024-09-20 14:55:10 -0400

    spaceRocks files
    for accessing among different PCs

---
+++ space_rocks/README.md
@@ -0,0 +1,4 @@
+# Scratch Space
+
+Use this directory to put code you are just trying out.  
+You can also save your notes here if you want.
---
+++ space_rocks/player.gd
@@ -0,0 +1,141 @@
+extends RigidBody2D
+
+signal shield_changed
+signal lives_changed
+signal dead
+
+@export var engine_power := 500
+@export var spin_power := 8000
+
+@export var bullet_scene: PackedScene
+@export var fire_rate = 0.25
+
+@export var max_shield = 100.0
+@export var shield_regen = 5.0
+
+@onready var shield_bar = $MarginContainer/HBoxContainer/ShieldBar
+
+var bar_textures = {
+"green": preload("res://assets/bar_green_200.png"),
+"yellow": preload("res://assets/bar_yellow_200.png"),
+"red": preload("res://assets/bar_red_200.png")
+}
+
+var shield = 0: set = set_shield
+var reset_pos = false
+var lives = 0: set = set_lives
+var can_shoot = true
+
+var screensize = Vector2.ZERO
+
+var thrust := Vector2.ZERO
+var rotation_dir := 0
+
+enum player_state { INIT, ALIVE, INVULNERABLE, DEAD }
+var state = player_state.INIT
+
+func _ready():
+	change_state(player_state.ALIVE)
+	screensize = get_viewport_rect().size
+	$GunCooldown.wait_time = fire_rate
+
+func change_state(new_state):
+	match new_state:
+		player_state.INIT:
+			$CollisionShape2D.set_deferred("disabled",true)
+			$Sprite2D.modulate.a = 0.5
+		player_state.ALIVE:
+			$CollisionShape2D.set_deferred("disabled",false)
+			$Sprite2D.modulate.a = 1.0
+		player_state.INVULNERABLE:
+			$CollisionShape2D.set_deferred("disabled",true)
+			$Sprite2D.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
+		player_state.DEAD:
+			$CollisionShape2D.set_deferred("disabled",true)
+			$Sprite2D.hide()
+			linear_velocity = Vector2.ZERO
+			dead.emit()
+	state = new_state
+
+func _process(delta):
+	get_input()
+	shield += shield_regen * delta
+
+
+func get_input():
+	thrust = Vector2.ZERO
+	if state in [player_state.DEAD, player_state.INIT] :
+		return
+	if Input.is_action_pressed("thrust"):
+		thrust = transform.x * engine_power
+	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
+
+func _physics_process(delta):
+	constant_force = thrust
+	constant_torque = rotation_dir * spin_power
+	
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
+	if reset_pos:
+		physics_state.transform.origin = screensize / 2
+		reset_pos = false
+
+func shoot():
+	if state == player_state.INVULNERABLE:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start($Muzzle.global_transform)
+
+
+func _on_gun_cooldown_timeout() -> void:
+	can_shoot = true
+
+
+func set_lives(value):
+	lives = value
+	lives_changed.emit(lives)
+	if lives <= 0:
+		change_state(player_state.DEAD)
+	else:
+		change_state(player_state.INVULNERABLE)
+	shield = max_shield
+
+func reset():
+	reset_pos = true
+	$Sprite2D.show()
+	lives = 3
+	change_state(player_state.ALIVE)
+
+func _on_invulnerability_timer_timeout():
+	change_state(player_state.ALIVE)
+
+func _on_body_entered(body):
+	if body.is_in_group("rocks"):
+		shield -= body.size * 25
+		body.explode()
+		
+		
+
+func explode():
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	$Explosion.hide()
+	
+func set_shield(value):
+	value = min(value, max_shield)
+	shield = value
+	shield_changed.emit(shield / max_shield)
+	if shield <= 0:
+		lives -= 1
+		explode()
+	shield = max_shield
