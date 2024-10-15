181ce00e76ab53bc578b8369ce9a0cca5e757a81
Bxdda (Chaughn) <cr1001404@wcupa.edu>
2024-10-15 10:32:04 -0400

    oct 11 update

--- jungle-jump/junglejump/game_state.gd
+++ jungle-jump/junglejump/game_state.gd
@@ -4,7 +4,7 @@ var num_levels = 1
 var current_level = 0
 
 var game_scene = "res://main.tscn"
-var title_screen = "res://ui/title.tscn"
+var title_screen = "res://screens/title_screen.tscn"
 
 func restart():
 	current_level = 0
@@ -13,6 +13,7 @@ func restart():
 func next_level():
 	current_level += 1
 	if current_level <= num_levels:
+		print("gmae")
 		get_tree().change_scene_to_file(game_scene)
 	else:
 		restart()
--- jungle-jump/junglejump/level_base.gd
+++ jungle-jump/junglejump/level_base.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 signal score_changed
 
+var door_scene = load("res://items/door.tscn")
 var item_scene = load("res://items/item.tscn")
 var score = 0: set = set_score
 
@@ -29,11 +30,23 @@ func spawn_items():
 	for cell in item_cells:
 		var data =$Items.get_cell_tile_data(0,cell)
 		var type = data.get_custom_data("type")
-		var item = item_scene.instantiate()
-		add_child(item)
-		item.init(type,$Items.map_to_local(cell))
-		item.picked_up.connect(self._on_item_picked_up)
+		if type == "door":
+			var door = door_scene.instantiate()
+			add_child(door)
+			door.position = $Items.map_to_local(cell)
+			door.body_entered.connect(_on_door_entered)
+		else:
+				var item = item_scene.instantiate()
+				add_child(item)
+				item.init(type,$Items.map_to_local(cell))
+				item.picked_up.connect(self._on_item_picked_up)
 
+func _on_door_entered(body):
+	GameState.next_level()
 
 func _on_item_picked_up():
 	score += 1
+
+
+func _on_player_died() -> void:
+	GameState.restart()
