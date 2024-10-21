ea72079d4f977f402e1e6fc5bda5d56124501522
Bxdda (Chaughn) <cr1001404@wcupa.edu>
2024-10-21 18:20:47 -0400

    jungle_jump finish
    finished by the book
    
    added moving platforms
    added ladders
    
    added several new features:
    GEM HUD
    player death animation (mario style)
    
    enemies can "snatch" items
    
    added launch pads
    
    moving platforms of diffrent directions and speeds
    added gems as a special score, added hidden gems that are shown by hidden triggers
    
    going to add:
    new enemy
    boss battle every 5 levels

--- jungle-jump/junglejump/game_state.gd
+++ jungle-jump/junglejump/game_state.gd
@@ -1,19 +1,24 @@
 extends Node
 
-var num_levels = 1
+var num_levels = 4
 var current_level = 0
 
 var game_scene = "res://main.tscn"
 var title_screen = "res://screens/title_screen.tscn"
+var game_over = "res://screens/game_over.tscn"
 
 func restart():
 	current_level = 0
 	get_tree().change_scene_to_file(title_screen)
 	
+
+func gameover():
+	get_tree().change_scene_to_file(game_over)
+	
+
 func next_level():
 	current_level += 1
-	if current_level <= num_levels:
-		print("gmae")
+	if current_level < num_levels:
+		print(current_level)
 		get_tree().change_scene_to_file(game_scene)
-	else:
-		restart()
+	
--- jungle-jump/junglejump/level_base.gd
+++ jungle-jump/junglejump/level_base.gd
@@ -1,17 +1,29 @@
 extends Node2D
 
 signal score_changed
+signal gems_changed
+
 
 var door_scene = load("res://items/door.tscn")
 var item_scene = load("res://items/item.tscn")
 var score = 0: set = set_score
+var gems = 0: set = set_gems
 
 
 func _ready(): 
 	$Items.hide()
 	$Player.reset($SpawnPoint.position)
 	set_camera_limits()
-	spawn_items()
+	connect_items()
+	connect_gems()
+	connect_triggers()
+	$ThemeMusic.play()
+	score = 100
+	$Door.body_entered.connect(_on_door_entered)
+	
+	
+	
+	
 
 
 func set_camera_limits():
@@ -19,34 +31,94 @@ func set_camera_limits():
 	var cell_size = $World.tile_set.tile_size
 	$Player/Camera2D.limit_left = (map_size.position.x - 5) * cell_size.x
 	$Player/Camera2D.limit_right = (map_size.end.x + 5) * cell_size.x
+	$Player/Camera2D.limit_bottom = 5000
+	$Player/Camera2D.limit_top = -2000
 	
 func set_score(value):
 	score = value
 	score_changed.emit(score)
 	
+	
 
-func spawn_items():
-	var item_cells = $Items.get_used_cells(0)
-	for cell in item_cells:
-		var data =$Items.get_cell_tile_data(0,cell)
-		var type = data.get_custom_data("type")
-		if type == "door":
-			var door = door_scene.instantiate()
-			add_child(door)
-			door.position = $Items.map_to_local(cell)
-			door.body_entered.connect(_on_door_entered)
-		else:
-				var item = item_scene.instantiate()
-				add_child(item)
-				item.init(type,$Items.map_to_local(cell))
-				item.picked_up.connect(self._on_item_picked_up)
+func set_gems(value):
+	gems = value
+	gems_changed.emit(gems)
+	
+	
+#func spawn_items():
+	#var item_cells = $Items.get_used_cells(0)
+	#for cell in item_cells:
+		#var data =$Items.get_cell_tile_data(0,cell)
+		#var type = data.get_custom_data("type")
+		#if type == "door":
+			#var door = door_scene.instantiate()
+			#add_child(door)
+			#print("creating door", type)
+			#door.position = $Items.map_to_local(cell)
+			#
+		#else:
+			#
+			#var item = item_scene.instantiate()
+			#add_child(item)
+			#print("creating ", type)
+			#item.init(type,$Items.map_to_local(cell))
+			#item.picked_up.connect(self._on_item_picked_up)
 
 func _on_door_entered(body):
 	GameState.next_level()
 
-func _on_item_picked_up():
-	score += 1
+func connect_items():
+	var items_group = $ItemsGroup
+	for child in items_group.get_children():
+		if child.has_signal("picked_up"):  # Check if the child has the "picked_up" signal
+			child.picked_up.connect(_on_item_picked_up)
+			
 
+func connect_gems():
+	var gems_group = $GemsGroup
+	var hidden_gems = $HiddenGems
+	for child in gems_group.get_children():
+		child.init("gem")
+		if child.has_signal("picked_up"):  # Check if the child has the "picked_up" signal
+			child.picked_up.connect(_on_gems_picked_up)
+	for child in hidden_gems.get_children():
+		child.init("gem")
+		child.hide()
+		if child.has_signal("picked_up"):  # Check if the child has the "picked_up" signal
+			child.picked_up.connect(_on_gems_picked_up)
+
+func connect_triggers():
+	var triggers_group = $Triggers
+	for child in triggers_group.get_children():
+		if child.has_signal("entered"):  
+			child.entered.connect(show_hidden)
+		if child.has_signal("died"):  
+			child.died.connect(show_hidden)
+	
+func _on_item_picked_up():
+	score += 50
+
+func _on_gems_picked_up():
+	gems += 1
 
 func _on_player_died() -> void:
-	GameState.restart()
+	await get_tree().create_timer(1.7).timeout
+	GameState.gameover()
+	
+
+func _on_ladders_body_entered(body: Node2D) -> void:
+	body.is_on_ladder = true
+
+
+func _on_ladders_body_exited(body: Node2D) -> void:
+	body.is_on_ladder = false
+
+
+func _on_enemy_died() -> void:
+	score += 100
+
+func show_hidden():
+	var hidden_gems = $HiddenGems
+	for child in hidden_gems.get_children():
+		child.visible = true
+		
